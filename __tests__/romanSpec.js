7d703a95536ef90f48b35199b14360bf9610b17d
Alienah <aidalbla@gmail.com>
2018-05-23 10:48:56 +0200

    red: number 3 converts to Roman number

--- __tests__/romanSpec.js
+++ __tests__/romanSpec.js
@@ -7,5 +7,6 @@ describe("Roman Numerals", () => {
     it("converts numbers to Roman numerals", () => {
         expect(app.fromNumber(1)).toBe("I");
         expect(app.fromNumber(2)).toBe("II");
+        expect(app.fromNumber(3)).toBe("III");
     });
 });
