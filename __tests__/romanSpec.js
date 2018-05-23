3c94d97335f9e5ca4f45adaa3f0a0ef35e9e9347
Alienah <aidalbla@gmail.com>
2018-05-23 10:43:48 +0200

    red: number 2 converts to Roman number

--- __tests__/romanSpec.js
+++ __tests__/romanSpec.js
@@ -6,6 +6,6 @@ describe("Roman Numerals", () => {
     });
     it("converts numbers to Roman numerals", () => {
         expect(app.fromNumber(1)).toBe("I");
-        
+        expect(app.fromNumber(2)).toBe("II");
     });
 });
